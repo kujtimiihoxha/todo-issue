c659128d47acc957a0587d67ea4d325cadbdc01f
kujtimiihoxha <kujtimii.h@gmail.com>
2016-02-12 14:52:42 +0100

    Changed paths in TodoFinderTest, it was failing in travis because the paths where only locally accessible.

--- src/test/java/com/kujtimhoxha/plugins/todo/finder/TodoFinderTest.java
+++ src/test/java/com/kujtimhoxha/plugins/todo/finder/TodoFinderTest.java
@@ -24,12 +24,12 @@ public class TodoFinderTest {
     @Test
     public void testFind() throws Exception {
         final List<File> files = new ArrayList<File>();
-        files.add(new File("/home/kujtimiihoxha/Projects/todo-issue/src/test/resources/finder/Test.html"));
-        files.add(new File("/home/kujtimiihoxha/Projects/todo-issue/src/test/resources/finder/Test.java"));
-        files.add(new File("/home/kujtimiihoxha/Projects/todo-issue/src/test/resources/finder/Test.js"));
-        files.add(new File("/home/kujtimiihoxha/Projects/todo-issue/src/test/resources/finder/Test.pl"));
-        files.add(new File("/home/kujtimiihoxha/Projects/todo-issue/src/test/resources/finder/Test.rb"));
-        files.add(new File("/home/kujtimiihoxha/Projects/todo-issue/src/test/resources/finder/Test.scss"));
+        files.add(new File(System.getProperty("user.dir")+"/src/test/resources/finder/Test.html"));
+        files.add(new File(System.getProperty("user.dir")+"/src/test/resources/finder/Test.java"));
+        files.add(new File(System.getProperty("user.dir")+"/src/test/resources/finder/Test.js"));
+        files.add(new File(System.getProperty("user.dir")+"/src/test/resources/finder/Test.pl"));
+        files.add(new File(System.getProperty("user.dir")+"/src/test/resources/finder/Test.rb"));
+        files.add(new File(System.getProperty("user.dir")+"/src/test/resources/finder/Test.scss"));
         final TodoFinder todoFinder = new TodoFinder(files);
         final List<Todo> todos=todoFinder.find();
         Assert.assertTrue(todos.size()==6);
